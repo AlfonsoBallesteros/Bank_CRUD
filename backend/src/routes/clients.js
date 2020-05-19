ffaebdea3c04d13d15cf9b21838bbd31f4ac9033
Alfonso Ballesteros <ponchob1258@gmail.com>
2020-05-18 20:43:57 -0500

    first commit

--- backend/src/routes/clients.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const router = Router();
-
-const { getClients } = require('../controllers/clients.controller')
-
-router.route('/')
-        .get(getClients)
-
-module.exports = router;
