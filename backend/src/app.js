d220565de705005ae828209a7760af51f64328e0
Alfonso Ballesteros <ponchob1258@gmail.com>
2020-06-04 15:54:55 -0500

    good

---
+++ backend/src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.set('port', process.env.PORT || 4000);
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/clientes', require('./routes/clients'));
+
+
+module.exports = app;
